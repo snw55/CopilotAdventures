ff68cfb06709ecb85c9ffd51ac82028b5858744c
HNO Mobile <heinis.mark@gmail.com>
2024-04-15 23:43:42 +0200

    Update reset.sh

--- .github/reset.sh
+++ .github/reset.sh
@@ -4,9 +4,15 @@
 git add .
 git commit -m "Submit new solution"
 
+# Push changes to the current branch
+git push origin HEAD
+
 # Switch to the main branch and pull the latest changes
 git checkout main
 git pull origin main
+
+# Revert all changes to match main
+git reset --hard origin/main
 
 # Generate a new branch name (e.g., based on date/time)
 BRANCH_NAME="participant-$(date +"%Y%m%d%H%M%S")"
@@ -15,7 +21,7 @@ BRANCH_NAME="participant-$(date +"%Y%m%d%H%M%S")"
 git checkout -b $BRANCH_NAME
 
 # Push the new branch to the remote repository
-git push origin $BRANCH_NAME
+git push --force origin $BRANCH_NAME
 
 # Switch back to the new branch
 git checkout $BRANCH_NAME
